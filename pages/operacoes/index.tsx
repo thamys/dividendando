d10ecba9033b446484cfae860b1934e11b5c868f
Thamyres Guedes <thataguedis@gmail.com>
2021-10-03 19:47:40 -0300

    Renomeei a pagina de cadastro para operações

---
+++ pages/operacoes/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AppLayout from "../../components/layout";
+import AddOperation from "../../components/add-operation";
+
+const Cadastro = () => {
+  return (
+    <AppLayout>
+      <AddOperation />
+    </AppLayout>
+  );
+};
+
+export default Cadastro;
